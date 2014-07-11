0f45f506d52ed5630c66ea4ab8767278287b25c1
palo <palo@toshiba.(none)>
2014-07-11 17:40:59 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,9 @@
-import os
 from setuptools import setup
-
-f = open(os.path.join(os.path.dirname(__file__), 'README.rst'))
-readme = f.read()
-f.close()
 
 setup(
     name='django-wysiwyg-redactor',
     version='0.3.9.1',
     description='django-wysiwyg-redactor is a lightweight responsive wysiwyg editor for Django',
-    long_description=readme,
     author="Douglas Miranda",
     author_email='douglasmirandasilva@gmail.com',
     url='https://github.com/douglasmiranda/django-wysiwyg-redactor',
